a4e6c9c23ca4e4511b1537c8dd54e4bee3de9afb
dawn <y.minn13@gmail.com>
2019-08-19 23:48:35 +0900

    dot star

--- exer/dummy.cpp
+++ exer/dummy.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 
+using namespace std;
+
 int main (void) {
-    int T, a, b;
-    std::cin >> T;
-    for (int tc = 1; tc <= T; tc++) {
-        std::cin >> a >> b;
-        std::cout << "#" << tc << " " << a / b << a % b << "\n";
+    cout.tie(0); ios_base::sync_with_stdio(0);
+    int N, i, j, core, a;
+    cin >> N;
+    core = N * 2 - 1;
+    for (i = 1-N; i < N; i++) {
+        a = N-1 - abs(i);
+        for (j = 0; j < core-a; j++) {
+            if (j < a) cout << " ";
+            else cout << "*";
+        }
+        cout << "\n";
     }
     return 0;
 }
