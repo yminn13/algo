1187430bc45e1fbb1a8fb2b7feb8d1b081cef608
dawn <y.minn13@gmail.com>
2019-08-23 22:39:52 +0900

    WILDCARD -- failed(2)

--- PS/ALGOSPOT/WILDCARD.cpp
+++ PS/ALGOSPOT/WILDCARD.cpp
@@ -1,7 +1,10 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 bool regexChecker (std::string pat, std::string str) {
     std::string::iterator pit, it;
+    std::string::size_type spos;
     bool isWildcard = false;
     it = str.begin();
     for (pit = pat.begin(); pit != pat.end(); pit++) {
@@ -14,8 +17,9 @@ bool regexChecker (std::string pat, std::string str) {
             std::cout << *pit << " " << *it << "\n";
 #endif
             if (isWildcard) {
-                it = std::find(it, str.end(), *pit);
-                if (it == str.end()) return false;
+                // it = std::find(it, str.end(), *pit);
+                spos = str.rfind(*it);
+                if (spos == std::string::npos) return false;
                 isWildcard = false;
                 it++;
             } else {
@@ -25,21 +29,29 @@ bool regexChecker (std::string pat, std::string str) {
             break;
         }
     }
+    if (!isWildcard && it != str.end()) return false;
     return true;
 }
 
 int main (void) {
     int tc, T, n, i;
     std::string pattern, str;
+    std::vector<std::string> fns;
+    std::vector<std::string>::iterator fit;
     std::cin >> T;
     for (tc = 1; tc <= T; tc++) {
         std::cin >> pattern >> n;
         for (i = 0; i < n; i++) {
             std::cin >> str;
             if (regexChecker(pattern, str)) {
-                std::cout << str << "\n";
+                fns.push_back(str);
             }
         }
+        std::sort (fns.begin(), fns.end());
+        for (fit = fns.begin(); fit != fns.end(); fit++) {
+            std::cout << *fit << "\n";
+        }
+        fns.clear();
     }
     return 0;
 }
