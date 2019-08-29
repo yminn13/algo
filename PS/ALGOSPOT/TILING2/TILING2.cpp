645a084b58177875c352abaed0824a3858e51354
dawn <y.minn13@gmail.com>
2019-08-30 02:59:25 +0900

    TILING2 add memorization

--- PS/ALGOSPOT/TILING2/TILING2.cpp
+++ PS/ALGOSPOT/TILING2/TILING2.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 int N;
+int cache[101];
 
 int tiling (int pc) {
-    if (pc+1 == N) return 1;
-    else if (pc+2 == N) return 2;
-    else {
-        // add = case and | case
-        return ((tiling(pc+1) + tiling(pc+2)) % 1000000007);
+    if (pc < 3) return pc;
+    else if (cache[pc] == -1) {
+        cache[pc] = ((tiling(pc-1) + tiling(pc-2)) % 1000000007);
     }
+    return cache[pc];
 }
 
 int main (void) {
     cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
     int T, tc, res;
+    memset(cache, -1, sizeof(cache));
+    cache[1] = 1;
+    cache[2] = 2;
     cin >> T;
     for (tc = 1; tc <= T; tc++) {
         cin >> N;
-        res = tiling(0);
+        res = tiling(N);
         cout << res << "\n";
     }
     return 0;
